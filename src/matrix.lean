ae47b233e35d70aad9a3c2e0d0b9c96d2169b305
anurudhp <anurudhp97@gmail.com>
2021-11-18 03:01:25 +0530

    add more quantum basics

---
+++ src/matrix.lean
@@ -0,0 +1,121 @@
+/-
+Copyright 2021 Anurudh Peduri 
+Licensed under the Apache License, Version 2.0 (check LICENSE).
+
+Authors: Anurudh Peduri 
+Some parts borrowed from https://github.com/duckki/lean-quantum/
+-/
+
+import data.complex.basic
+import data.complex.is_R_or_C
+import algebra.star.basic
+import data.matrix.basic
+open_locale complex_conjugate
+
+------------------------------------------------------------------------------
+-- Convenience definition for real/complex numbers
+
+notation |x| := is_R_or_C.abs x
+
+------------------------------------------------------------------------------
+-- Convenience definitions for complex matrix
+
+@[reducible]
+def Matrix (m n : ℕ) := matrix (fin m) (fin n) ℂ
+
+namespace Matrix
+
+variables {m n : ℕ}
+
+-- Definition of `adjoint` or `†` (dagger) operator.
+@[simp]
+def adjoint (M : Matrix m n) : Matrix n m | x y := conj (M y x)
+
+end Matrix
+
+notation `Vector` n := Matrix n 1
+notation `RVector` n := Matrix 1 n
+notation `Square` n := Matrix n n
+notation `I` n := (1 : Matrix n n)
+
+infixl `⬝`:80 := matrix.mul
+postfix `†`:100 := Matrix.adjoint
+
+
+------------------------------------------------------------------------------
+-- Vector properties
+
+namespace Matrix
+
+variables {n : ℕ} (s : Vector n)
+
+-- The property of "unit" Vectors.
+def unit := s† ⬝ s = 1
+
+end Matrix
+
+
+------------------------------------------------------------------------------
+-- Square matrix properties
+
+namespace Matrix
+
+variables {n : ℕ} (M : Square n)
+
+def normal := M† ⬝ M = M ⬝ M†
+
+def unitary := M† ⬝ M = 1
+
+def hermitian := M† = M
+
+def projector := M ⬝ M = M
+
+end Matrix
+
+
+------------------------------------------------------------------------------
+-- Standard basis `std_basis` for a column vector with a single value
+section std_basis
+
+variables {m : Type*} [fintype m]
+variables [decidable_eq m]
+
+-- column vector with a single value at a given row
+@[simp]
+def std_basis (i : m) : matrix m (fin 1) ℂ
+:= λ j _, if j = i then 1 else 0
+
+end std_basis
+
+
+------------------------------------------------------------------------------
+-- Kronecker product definition
+
+section kron
+
+variables {m n p q : ℕ}
+
+-- kron_div: `i / p` : `fin m`
+@[reducible]
+def kron_div (i : fin (m*p)) : fin m := ⟨ (i : ℕ)/p, sorry⟩
+
+-- kron_mod: `i % p` : `fin p`
+@[reducible]
+def kron_mod (i : fin (m*p)) : fin p := ⟨(i : ℕ)%p, sorry⟩
+
+-- Kronecker product
+def kron (A : Matrix m n) (B : Matrix p q) : Matrix (m*p) (n*q)
+:= λ i j,   -- A (i/p) (j/q) * B (i%p) (j%q)
+      A (kron_div i) (kron_div j)
+    * B (kron_mod i) (kron_mod j)
+
+-- kron_loc: `m * r + v : fin (m * p)
+-- Reverses `(r, v)` back to `x`, where `r = kron_div x` and `v = kron_mod x`.
+@[reducible]
+def kron_loc (r : fin m) (v : fin p) : fin (m * p) :=
+  ⟨p * (r : ℕ) + (v : ℕ), sorry⟩
+
+end kron
+
+infixl ` ⊗ `:75 := kron
+
--- src/quantum.lean
+++ src/quantum.lean
@@ -1,19 +1,150 @@
-import data.matrix.basic
-import data.complex.basic
-open matrix
-open_locale complex_conjugate
+/-
+Copyright 2021 Anurudh Peduri
+Licensed under the Apache License, Version 2.0 (check LICENSE).
 
--- `n`-qubit operator
-abbreviation QubitState (n : ℕ) := fin (2^n) → ℂ
-abbreviation QubitOp (n : ℕ) := matrix (fin (2^n)) (fin (2^n)) ℂ
+Authors: Anurudh Peduri
+Some parts borrowed from https://github.com/duckki/lean-quantum/
+-/
 
--- `n`-qubit Identity operator
-def I {n : ℕ} : QubitOp n := λ i j, if i = j then 1 else 0
+import matrix
+import data.matrix.notation
 
--- dagger of an `n`-qubit operator
-def dagger {n : ℕ} (A : QubitOp n) : QubitOp n := λ i j, conj (A j i)
+open_locale big_operators
+open Matrix
 
-def Hermitian {n : ℕ} (H : QubitOp n) : Prop := H = dagger H 
-def Unitary {n : ℕ} (U : QubitOp n) : Prop := dagger U * U = @I n
+------------------------------------------------------------------------------
+-- Quantum Definitions
 
-def applyOp {n : ℕ} (A : QubitOp n) (ψ : QubitState n) : QubitState n := mul_vec A ψ
+abbreviation Ket (n : ℕ) := Vector (2^n)
+abbreviation Bra (n : ℕ) := RVector (2^n)
+abbreviation Op (n : ℕ) := Square (2^n)
+
+------------------------------------------------------------------------------
+-- Matrix helpers for measurement definitions
+
+namespace Matrix
+
+variables {n m : ℕ} (s : Vector n)
+
+/-- Projection operator onto the state `s` (aka "observable").
+`proj s` can be read as `|ψ⟩⟨ψ|`, when `|ψ⟩ = s`. -/
+def proj : Square n := s ⬝ (s†)
+
+/-- Trace of square matrix -/
+def trace (A : Square n) : ℂ := ∑ i, A i i
+
+notation `Tr(` x `)` := trace x
+
+/-- `n × n` partial traces of `m × m` subcomponents of
+`(n * m) × (n * m)` square matrix. -/
+def partial_trace (M : Square (n * m)) : Square n
+:= λ i j, ∑ k, M (kron_loc i k) (kron_loc j k)
+
+notation `Tr'(` x `)` := partial_trace x
+
+end Matrix
+
+namespace Matrix
+end Matrix
+
+------------------------------------------------------------------------------
+-- Common states
+notation `√2` := real.sqrt 2
+notation `ι` := complex.I
+
+-- |0⟩ and |1⟩ using `std_basis`
+def ket0 : Ket 1 := ![![1], ![0]]
+def ket1 : Ket 1 := ![![0], ![1]]
+def bra0 : Bra 1 := ket0†
+def bra1 : Bra 1 := ket1†
+
+-- hadamard basis
+noncomputable def ketplus : Ket 1 := ![![1/√2], ![1/√2]]
+noncomputable def ketminus : Ket 1 := ![![1/√2], ![-1/√2]]
+noncomputable def braplus : Bra 1 := ketplus†
+noncomputable def braminus : Bra 1 := ketminus†
+
+notation `|0⟩` := ket0
+notation `|1⟩` := ket1
+notation `⟨0|` := bra0
+notation `⟨1|` := bra1
+notation `|+⟩` := ketplus
+notation `|-⟩` := ketminus
+notation `⟨+|` := braplus
+notation `⟨-|` := braminus
+
+-- |00...0⟩ (= |0⟩ ⊗ ... ⊗ |0⟩ or the `n`-th tensor power of |0⟩).
+-- Used for zero padding or ancillae qubits.
+def ket_zeros (n : ℕ) : Ket n := std_basis ⟨0, by simp⟩
+
+------------------------------------------------------------------------------
+-- Common single qubit unitaries
+
+-- X gate (aka NOT gate)
+def σX : Op 1 := ![
+    ![ 0, 1 ],
+    ![ 1, 0 ]]
+
+def σY : Op 1 := ![
+    ![ 0, ι ],
+    ![ -ι, 0 ]]
+
+def σZ : Op 1 := ![
+    ![ 1, 0 ],
+    ![ 0, -1 ]]
+
+-- Hadamard gate
+noncomputable
+def H : Op 1 := ![
+    ![ 1/√2,  1/√2 ],
+    ![ 1/√2, -1/√2 ]]
+
+-- Controlled-NOT gate (aka CX gate)
+def CNOT : Op 2 := ![
+    ![ 1, 0, 0, 0 ],
+    ![ 0, 1, 0, 0 ],
+    ![ 0, 0, 0, 1 ],
+    ![ 0, 0, 1, 0 ]]
+
+-- Controlled-Z gate
+def CZ : Op 2 := ![
+    ![ 1, 0, 0, 0 ],
+    ![ 0, 1, 0, 0 ],
+    ![ 0, 0, 1, 0 ],
+    ![ 0, 0, 0, -1 ]]
+
+def SWAP : Op 2 :=
+    ![ ![1, 0, 0, 0],
+       ![0, 0, 1, 0],
+       ![0, 1, 0, 0],
+       ![0, 0, 0, 1]]
+
+
+------------------------------------------------------------------------------
+-- Controlled-U gates
+
+/-- Controlled-U : |0⟩⟨0| ⊗ I + |1⟩⟨1| ⊗ U -/
+def controlled {n : ℕ} (U : Op n) : Op (n + 1) := |0⟩⬝⟨0| ⊗ (I _) + |1⟩⬝⟨1| ⊗ U
+/-- Controlled'-U : |1⟩⟨1| ⊗ I + |0⟩⟨0| ⊗ U -/
+def controlled' {n : ℕ} (U : Op n) : Op (n + 1) := (σX ⊗ I _) ⬝ controlled U ⬝ (σX ⊗ I _)
+
+------------------------------------------------------------------------------
+-- Common projection operators
+
+def P0 : Op 1 := ![
+    ![ 1, 0 ],
+    ![ 0, 0 ]]
+
+def P1 : Op 1 := ![
+    ![ 0, 0 ],
+    ![ 0, 1 ]]
+
+noncomputable
+def P_plus : Op 1 := ![
+    ![ 1/2, 1/2 ],
+    ![ 1/2, 1/2 ]]
+
+noncomputable
+def P_minus : Op 1 := ![
+    ![ 1/2, -(1/2) ],
+    ![ -(1/2), 1/2 ]]
